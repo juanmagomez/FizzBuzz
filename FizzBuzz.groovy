bbb7333e42ad66741699e4472a47d327227820ed
Juanma Gomez <slither_new@hotmail.com>
2012-03-03 18:25:55 +0100

    Included setUp method execution before each test

--- FizzBuzz.groovy
+++ FizzBuzz.groovy
@@ -1,18 +1,25 @@
 import org.junit.Test
+import org.junit.Before
+
 import static org.junit.Assert.*
 
 class FizzBuzzTest {
 
+	FizzBuzz fizzBuzz
+
+	@Before
+	public void setUp() {
+		fizzBuzz = new FizzBuzz()
+	}
+
 	@Test
 	public void "test with number 1"() {
-		FizzBuzz fizzBuzz = new FizzBuzz()
 		Integer numberOne = new Integer(1)
 		assertEquals(1, fizzBuzz.returnValue(numberOne))
 	}
 
 	@Test
 	public void "test with number 2"() {
-		FizzBuzz fizzBuzz = new FizzBuzz()
 		Integer numberTwo = new Integer(2)
 		assertEquals(2, fizzBuzz.returnValue(numberTwo))
 	}
