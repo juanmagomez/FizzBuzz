f7df56f8fcdf00bd1c21c921b0bc0479f874b06b
Juanma Gomez <slither_new@hotmail.com>
2012-03-03 18:22:28 +0100

    Refactor to include JUnit 4

--- FizzBuzz.groovy
+++ FizzBuzz.groovy
@@ -1,18 +1,22 @@
-import groovy.util.GroovyTestCase
+import org.junit.Test
+import static org.junit.Assert.*
 
-class FizzBuzzTest extends GroovyTestCase {
+class FizzBuzzTest {
 
+	@Test
 	public void "test with number 1"() {
 		FizzBuzz fizzBuzz = new FizzBuzz()
 		Integer numberOne = new Integer(1)
 		assertEquals(1, fizzBuzz.returnValue(numberOne))
 	}
 
+	@Test
 	public void "test with number 2"() {
 		FizzBuzz fizzBuzz = new FizzBuzz()
 		Integer numberTwo = new Integer(2)
 		assertEquals(2, fizzBuzz.returnValue(numberTwo))
 	}
+
 
 }
 
